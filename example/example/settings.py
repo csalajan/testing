190cf1831a02f13bc702d4f7be49c028c1e4c886
Jordan Enterkin <jordan.a.enterkin@gmail.com>
2013-07-01 00:23:31 -0400

    fixed issue with permissions

--- example/example/settings.py
+++ example/example/settings.py
@@ -175,8 +175,8 @@ LOGGING = {
 }
 
 # API base permissions
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
-}
+#REST_FRAMEWORK = {
+#    'DEFAULT_PERMISSION_CLASSES': (
+#        'rest_framework.permissions.IsAuthenticated',
+#    )
+#}
